2fd86d62a0cd80e33a7844be556941a82c1c0614
Cupelt <57065968+Cupelt@users.noreply.github.com>
2024-03-27 10:01:54 +0900

    fix exit_every_server does not loged in

--- src/exit_every_server.js
+++ src/exit_every_server.js
@@ -1,9 +1,15 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+require('dotenv').config()
 
 client.once(Events.ClientReady, readyClient => {
     const guilds = client.guilds.cache;
     guilds.forEach(guild => guild.leave());
 
+    console.log("exit succesfuly done.");
+
     client.destroy();
 });
+
+client.login(process.env.TOKEN);
+
