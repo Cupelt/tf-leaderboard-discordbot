bcf94ad4e6ff4532a3ac8168e20a5c9c710d04ac
Cupelt <57065968+Cupelt@users.noreply.github.com>
2024-03-30 03:06:42 +0900

    disable wirte log files

--- src/logger.js
+++ src/logger.js
@@ -1,15 +1,10 @@
 const winston = require("winston");
-const winstonDaily = require("winston-daily-rotate-file"); 
 const path = require("path");
 
 const { createLogger, transports } = require("winston"); 
 const process = require("process");
 
-const logDir = `${path.join(__dirname, '..')}/logs`;
-
 const { combine, timestamp, label, printf } = winston.format;
-
-
 
 const logFormat = printf(({ level, message, label, timestamp }) => {
 
@@ -20,23 +15,7 @@ const logFormat = printf(({ level, message, label, timestamp }) => {
 const logger = createLogger({
     format: combine(label({ label: "THE_FINALS_CNS" }), timestamp(), logFormat),
     transports: [
-        new transports.Console(),
-        new winstonDaily({
-            level: "info", 
-            datePattern: "YYYY-MM-DD", 
-            dirname: logDir,
-            filename: "%DATE%.log",
-            maxSize: "20m",
-            maxFiles: "30d",
-        }),
-        new winstonDaily({
-            level: "error", 
-            datePattern: "YYYY-MM-DD",
-            dirname: logDir, 
-            filename: "%DATE%.error.log",
-            maxSize: "20m",
-            maxFiles: "30d", 
-        }),
+        new transports.Console()
     ],
 });
 
