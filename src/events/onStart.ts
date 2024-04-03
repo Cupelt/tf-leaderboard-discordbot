7d865bd6bccf7d719fe7cf1e947b8d6197cdf563
Cupelt <57065968+Cupelt@users.noreply.github.com>
2024-04-03 13:03:29 +0900

    add activities

--- src/events/onStart.ts
+++ src/events/onStart.ts
@@ -1,4 +1,4 @@
-import { Client, Events } from "discord.js";
+import { ActivityType, Client, Events } from "discord.js";
 import { BotEvent } from "../@types/client";
 import logger from "../utils/logger";
 
@@ -6,7 +6,15 @@ const event : BotEvent = {
     name: Events.ClientReady,
     once: true,
     execute: (client : Client) => {
-        logger.info(`Logged in as ${client.user?.tag}`)
+        logger.info(`Logged in as ${client.user?.tag}`);
+
+        client.user?.setPresence({
+			activities: [{
+				name: 'Hacking TheFinals Protocol',
+				type : ActivityType.Competing
+			}],
+			status: 'online'
+		})
     }
 }
 
