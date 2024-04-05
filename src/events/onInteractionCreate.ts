62eb7ce662aa6806f50f6eb72ff6b7c9cf411561
Cupelt <guswnd8800@naver.com>
2024-04-05 09:05:39 +0900

    add AutoComplete

--- src/events/onInteractionCreate.ts
+++ src/events/onInteractionCreate.ts
@@ -1,16 +1,27 @@
 import { Events, Interaction } from "discord.js";
 import { BotEvent } from "../@types/client";
 import logger from "../utils/logger";
+import { forEachLeadingCommentRange } from "typescript";
 
 const event: BotEvent = {
     name: Events.InteractionCreate,
     execute: async (interaction: Interaction) => {
-        if (!interaction.isChatInputCommand()) return;
+        if (!interaction.isChatInputCommand() && !interaction.isAutocomplete()) return;
         const command = interaction.client.commands.get(interaction.commandName);
 
         if (!command) {
             logger.error(`No command matching ${interaction.commandName} was found`);
             return;
+        }
+
+        if (interaction.isAutocomplete()) {
+            try {
+                await command.autocomplete?.(interaction);
+            } catch (error: any) {
+                logger.error(error.stack);
+            } finally {
+                return;
+            }
         }
 
         try {
