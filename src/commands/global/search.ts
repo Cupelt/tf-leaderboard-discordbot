d43378f31f6e2744f5d7c384544d3a0c9cff556b
Cupelt <57065968+Cupelt@users.noreply.github.com>
2024-06-12 10:12:47 +0900

    feat: change ui

--- src/commands/global/search.ts
+++ src/commands/global/search.ts
@@ -10,6 +10,7 @@ import {
     TextInputStyle, 
     ButtonComponent,
     ModalSubmitInteraction,
+    inlineCode,
 } from 'discord.js'
 
 import { request } from 'undici'
@@ -22,15 +23,15 @@ function createEmbed(data: UserData): EmbedBuilder {
 
     return new EmbedBuilder()
         .setColor(rank_color[Math.floor((data.leagueNumber - 1) / 4)])
-        .setTitle(`#${data.rank} - 『${data.steamName != "" ? data.steamName : data.xboxName != "" ? data.xboxName : data.psnName}』`)
-        .setAuthor({ name: `TheFinals-CNS`, iconURL: `https://cdn.discordapp.com/avatars/1219832567570890833/889af2fc8b96fc95cf833a4395092813.webp?size=1024` })
+        .setTitle(`${data.name}`)
         .setThumbnail(`https://storage.googleapis.com/embark-discovery-leaderboard/img/thumbs/${data.league.toLowerCase().replaceAll(" ", "-")}-thumb.png`)
         .addFields(
-            { name: '\u200B', value: '\u200B' },
-            { name: ' ═══•°• 랭크 •°•═══', value: `\`\`\`${data.league}\`\`\``, inline: true },
-            { name: ' ══•°• 24시간 •°•══', value: `\`\`\`diff\n${(data.change > 0) ? ("+"+data.change) : data.change}\n\`\`\``, inline: true },
+            { name: '랭크', value: inlineCode(`${data.league}`), inline: true },
+            { name: '순위', value: inlineCode(`${data.rank}`), inline: true },
+            { name: '변동률', value: inlineCode(`${(data.change > 0) ? ("+"+data.change) : data.change}`), inline: true },
         )
-        .setFooter({ text: `${data.name}`, iconURL: 'https://cdn2.steamgriddb.com/logo/3dcf08bb1312cd37914637289ba97421.png' });
+        .setFooter({ text: `TheFinals-CNS`, iconURL: `https://cdn.discordapp.com/avatars/1219832567570890833/889af2fc8b96fc95cf833a4395092813.webp?size=1024` })
+        .setTimestamp()
 }
 
 function createPageButton(data: UserData ,now_page: number, page_length: number): ActionRowBuilder<ButtonBuilder> {
@@ -38,7 +39,7 @@ function createPageButton(data: UserData ,now_page: number, page_length: number)
         .setCustomId('page_count')
         .setLabel(`${now_page + 1} / ${page_length}`)
         .setStyle(ButtonStyle.Secondary)
-        .setDisabled(page_length === 1);
+        .setDisabled(true/*page_length === 1*/);
 
     const btn_symbols = [
         {symbol: '⏪', page: -10},
@@ -113,47 +114,47 @@ const command: SlashCommand = {
             const component: ButtonComponent = collected_interaction.component as ButtonComponent;
 
             if (component.customId === 'page_count') {
-                const modal = new ModalBuilder()
-                    .setCustomId(`move_page_${interaction.user.id}`)
-                    .setTitle('페이지 이동');
+                // const modal = new ModalBuilder()
+                //     .setCustomId(`move_page_${interaction.user.id}`)
+                //     .setTitle('페이지 이동');
                 
-                const page_input = new TextInputBuilder()
-                    .setCustomId(`inputed_page`)
-                    .setMaxLength(5)
-                    .setMinLength(1)
-                    .setLabel("이동할 페이지를 입력하세요.")
-                    .setPlaceholder(`1~${page_length} / 현재 페이지 ${now_page + 1}`)
-                    .setStyle(TextInputStyle.Short)
-                    .setRequired(true);
+                // const page_input = new TextInputBuilder()
+                //     .setCustomId(`inputed_page`)
+                //     .setMaxLength(5)
+                //     .setMinLength(1)
+                //     .setLabel("이동할 페이지를 입력하세요.")
+                //     .setPlaceholder(`1~${page_length} / 현재 페이지 ${now_page + 1}`)
+                //     .setStyle(TextInputStyle.Short)
+                //     .setRequired(true);
                 
-                await collected_interaction.showModal(modal.addComponents(new ActionRowBuilder().addComponents(page_input) as ActionRowBuilder<TextInputBuilder>));
+                // await collected_interaction.showModal(modal.addComponents(new ActionRowBuilder().addComponents(page_input) as ActionRowBuilder<TextInputBuilder>));
 
-                let is_modal_faild = false;
-                await collected_interaction
-                    .awaitModalSubmit({ filter: (i) => i.customId === `move_page_${i.user.id}`, time: 20_000 })
-                    .then((modal_interaction: ModalSubmitInteraction) => {
-                        const inputed_page = modal_interaction.fields.getTextInputValue('inputed_page');
+                // let is_modal_faild = false;
+                // await collected_interaction
+                //     .awaitModalSubmit({ filter: (i) => i.customId === `move_page_${i.user.id}`, time: 20_000 })
+                //     .then(async (modal_interaction: ModalSubmitInteraction) => {
+                //         const inputed_page = modal_interaction.fields.getTextInputValue('inputed_page');
 
-                        const regex = /^0*[1-9]\d*$/;
-                        if (regex.test(inputed_page) && Number(inputed_page) <= page_length) {
-                            now_page = Number(inputed_page) - 1;
-                            data = leaderboardData.data[now_page];
+                //         const regex = /^0*[1-9]\d*$/;
+                //         if (regex.test(inputed_page) && Number(inputed_page) <= page_length) {
+                //             now_page = Number(inputed_page) - 1;
+                //             data = leaderboardData.data[now_page];
 
-                            collected_interaction.update({
-                                embeds: [createEmbed(data)],
-                                components: [createPageButton(data, now_page, page_length)],
-                            });
-                        } else {
-                            modal_interaction.reply({ content: `1 부터 ${page_length} 까지의 숫자만 입력해 주세요!`, ephemeral: true });
-                            is_modal_faild = true;
-                        }
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                        is_modal_faild = true;
-                    });
+                //             await collected_interaction.update({
+                //                 embeds: [createEmbed(data)],
+                //                 components: [createPageButton(data, now_page, page_length)],
+                //             });
+                //         } else {
+                //             await modal_interaction.reply({ content: `1 부터 ${page_length} 까지의 숫자만 입력해 주세요!`, ephemeral: true });
+                //             is_modal_faild = true;
+                //         }
+                //     })
+                //     .catch((err) => {
+                //         console.log(err);
+                //         is_modal_faild = true;
+                //     });
                 
-                if (is_modal_faild) return;
+                // if (is_modal_faild) return;
 
             } else {
                 now_page += Number(component.customId?.replaceAll('page_count_', ""));
